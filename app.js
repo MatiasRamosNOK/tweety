c6a67b3f3f61690a64324131d0c95f5d39b94512
FlaviaCohen <flaviacohen1988@gmail.com>
2020-05-13 11:26:31 -0300

    live test

--- app.js
+++ app.js
@@ -3,39 +3,42 @@ const chalk = require("chalk");
 const morgan = require("morgan");
 const app = express();
 const nunjucks = require("nunjucks");
+const routes = require('./routes');
 
-var users = {
-  0: "Matias",
-  1: "Flavia",
-};
+app.use('/', routes);
 
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms")
+ morgan(":method :url :status :res[content-length] - :response-time ms")
 );
+
+app.use(express.static('public'))
 // hacé tu logueo acá.
 // llamá a `next`. Sino tu app recibirá pedidos
 // pero no responderá adecuadamente.
 
-app.get("/special/", function (req, res) {
+
+/* app.get("/special/", function (req, res) {
   res.send("Hola estas en special");
   next();
-});
+}); */
 
-app.get("/", function (req, res) {
-  const people = [{ name: "Full" }, { name: "Stacker" }, { name: "Son" }];
+/* app.get("/", function (req, res) {
+  const people = [{ name: "Matías" }, { name: "Flavia" }, { name: "Plataforma5" }, { name: "Jesús"}];
   res.render("index", { title: "Hall of Fame", people: people });
-});
-app.get("/is-anybody-in-there", function (req, res) {});
-app.post("/modernism", function (req, res) {});
+}); */
+
+/* app.get("/is-anybody-in-there", function (req, res) {});
+app.post("/modernism", function (req, res) {}); */
 
 app.listen(3001, function () {
-  console.log("Example app listening on port 3000!");
+  console.log("Example app listening on port 3001!");
 });
 
-var locals = {
+/* var locals = {
   title: "An Example",
   people: [{ name: "Gandalf" }, { name: "Frodo" }, { name: "Hermione" }],
-};
+}; */
+
 nunjucks.configure("views", { noCache: true });
 
 app.set("view engine", "html"); // hace que res.render funcione con archivos html
---
+++ tweetBank.js
@@ -0,0 +1,37 @@
+const _ = require('lodash')
+var data = []
+
+function add (name, content) {
+    data.push({ name: name, content: content });
+  }
+  
+function list () {
+return _.cloneDeep(data);
+}
+
+function find (properties) {
+    return _.cloneDeep(_.filter(data, properties));
+}
+  
+module.exports = { add: add, list: list, find: find };
+
+/* ****************** */
+
+const randArrayEl = function(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  };
+  const getFakeName = function() {
+    const fakeFirsts = ['Toni', 'Guille', 'Santi', 'Facu', 'Alan', 'Pinki', 'Tincho', 'Solano', 'R2D2'];
+    const fakeLasts = ['Scanlan', 'Aszyn', 'Tralice', 'Velasco', 'Sainz', 'Palacio', 'Palacios', 'Lidueña', 'Fisicaro', 'Ecma'];
+    return randArrayEl(fakeFirsts) + " " + randArrayEl(fakeLasts);
+  };
+  const getFakeTweet = function() {
+    const awesome_adj = ['increibles', 'emocionantes', 'increibles', 'graciosos', 'dulces', 'cool',  'sorprendentes', 'impresionantes'];
+    return "Plataforma 5 es " + randArrayEl(awesome_adj) + "! Los profesores simplemente son " + randArrayEl(awesome_adj) + ". #P5Love #codedreams";
+  };
+  for (let i = 0; i < 10; i++) {
+    module.exports.add( getFakeName(), getFakeTweet() );
+  }
+  
+  add("Flavia", "mi primer tweet")
+  console.log(find({name: "Flavia"}))
